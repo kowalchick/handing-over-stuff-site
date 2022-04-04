c77595bdf667872f36417e7ae5a0f3b3a5688e44
kowalchick <kkowalczyk91@gmail.com>
2022-04-04 11:40:22 +0200

    Finish structure of NavMenu component

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -11,19 +11,19 @@ function NavMenu() {
                 <div className="nav-top">
                     <div className="nav-user">
                         <>
-                                    <h3>Cześć</h3>
-                                    <NavLink to='/oddaj-rzeczy/'>Oddaj rzeczy</NavLink>
-                                            <Scroll to={"survey-form"} spy={true} smooth={true} duration={500} >
-                                                {"Formularz"}
-                                            </Scroll>
-                                    <NavLink to="/">Home</NavLink>
-                                    <button>Logout</button>
-                                </>
-                                <>
-                                    <NavLink to="/">Home</NavLink>
-                                    <NavLink to='/logowanie/'>Zaloguj</NavLink>
-                                    <NavLink to='/rejestracja/'>Załóż konto</NavLink>
-                                </>
+                            <h3>Cześć</h3>
+                            <NavLink to='/oddaj-rzeczy/'>Oddaj rzeczy</NavLink>
+                                    <Scroll to={"survey-form"} spy={true} smooth={true} duration={500} >
+                                        {"Formularz"}
+                                    </Scroll>
+                            <NavLink to="/">Home</NavLink>
+                            <button>Logout</button>
+                        </>
+                        <>
+                            <NavLink to="/">Home</NavLink>
+                            <NavLink to='/logowanie/'>Zaloguj</NavLink>
+                            <NavLink to='/rejestracja/'>Załóż konto</NavLink>
+                        </>
                     </div>
 
                     <nav className="nav">
