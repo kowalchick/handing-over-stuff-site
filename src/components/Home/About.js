13e23111b3bb358da07f8d05ae2544b423379d6f
kowalchick <kkowalczyk91@gmail.com>
2022-04-04 11:38:03 +0200

    Add logic of About component

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Signature from '../../assets/Signature.svg';
 
 function About() {
     return (
-        <div></div>
+        <section className="about">
+            <div className="container">
+                <div className="about-wrapper">
+                    <div className="about-content">
+                        <div className="about-title">
+                            <h2>O nas</h2>
+                        </div>
+                        <p className="about-text">
+                            Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya
+                            nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                        </p>
+                        <div className="about-signature">
+                            <img src={Signature} alt="Signature"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
