b769da21ecc1ffc4c2ebe13e3736ce1dd8b36bfc
kowalchick <kkowalczyk91@gmail.com>
2022-04-04 12:46:25 +0200

    Add FooterForm basic structure

--- src/components/FooterForm.js
+++ src/components/FooterForm.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function FooterForm() {
+const FooterForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div></div>
+        <footer className="footer" >
+            <div className="container">
+                <div className="contact-wrapper">
+                    <div className="section-contact">
+                        <div className="section-contact-title">
+                            <h2>Skontaktuj się z nami</h2>
+                        </div>
+                        <form className="section-contact-form" onSubmit={handleSubmit}>
+                            <div className="input-wrapper">
+                                <label className="form-label" htmlFor="name">Wpisz swoje imię</label>
+                                <input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    placeholder="Jan"
+                                    value={name}
+                                    onChange={e => setName(e.target.value)}
+                                />
+                            </div>
+                            <div className="input-wrapper">
+                                <label className="form-label" htmlFor="email">Wpisz swój email</label>
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="jankowalski@gmail.pl"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <label className="form-label" htmlFor="message">Wpisz swoją wiadomość</label>
+                            <textarea
+                                rows="4"
+                                id="message"
+                                name="message"
+                                placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                                value={message}
+                                onChange={e => setMessage(e.target.value)}
+                            />
+                            <div className="submit-wrapper">
+                                <button type="submit">Wyślij</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 }
 
