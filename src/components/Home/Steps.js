895d29f59c66c51239f3cca6a003b891ad405125
kowalchick <kkowalczyk91@gmail.com>
2022-04-04 11:59:35 +0200

    Finish structure Steps component

--- src/components/Home/Steps.js
+++ src/components/Home/Steps.js
@@ -1,8 +1,81 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import icon1 from '../../assets/Icon-1.svg';
+import icon2 from '../../assets/Icon-2.svg';
+import icon3 from '../../assets/Icon-3.svg';
+import icon4 from '../../assets/Icon-4.svg';
 
-function Steps() {
+const Steps = () => {
     return (
-        <div></div>
+        <section className="steps">
+            <div className="container">
+                <div className="steps-wrapper">
+                    <div className="steps-title">
+                        <h2>Wystarczą 4 proste kroki</h2>
+                    </div>
+                    <div className="steps-steps">
+                        <div className="steps-2">
+                            <div className="step">
+                                <div className="step-icon">
+                                    <img src={icon1} alt="tshirt"/>
+                                </div>
+                                <div className="step-title">
+                                    <h3>Wybierz rzeczy</h3>
+                                </div>
+                                <div className="step-subtitle">
+                                    <p>
+                                        ubrania, zabawki, sprzęt i inne
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="step">
+                                <div className="step-icon">
+                                    <img src={icon2} alt="shopping tag"/>
+                                </div>
+                                <div className="step-title">
+                                    <h3>Spakuj je</h3>
+                                </div>
+                                <div className="step-subtitle">
+                                    <p>
+                                        skorzystaj z worków na śmieci
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="steps-2">
+                            <div className="step">
+                                <div className="step-icon">
+                                    <img src={icon3} alt="hand glass"/>
+                                </div>
+                                <div className="step-title">
+                                    <h3>Zdecyduj komu chcesz pomóc</h3>
+                                </div>
+                                <div className="step-subtitle">
+                                    <p>
+                                        wybierz zaufane miejsce
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="step">
+                                <div className="step-icon">
+                                    <img src={icon4} alt="cycle"/>
+                                </div>
+                                <div className="step-title">
+                                    <h3>Zamów kuriera</h3>
+                                </div>
+                                <div className="step-subtitle">
+                                    <p>
+                                        kurier przyjedzie w dogodnym terminie
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Link to="/oddaj-rzeczy/">Oddaj<br/>rzeczy</Link>
+                </div>
+            </div>
+        </section>
     );
 }
 
