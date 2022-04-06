8b91e0eb4f4ec6db08e773a18716725f855eba5d
kowalchick <kkowalczyk91@gmail.com>
2022-04-06 11:38:17 +0200

    Add logout component structure

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import NavMenu from "./NavMenu";
+import {Link} from "react-router-dom";
 
-function Logout(props) {
+const Logout = () => {
     return (
-        <div>Your session was closed successfully!</div>
+        <>
+            <NavMenu/>
+            <section className="logout">
+                <div className="logout-title">
+                    <h1 className="fancy">You have been logged out successfully</h1>
+                </div>
+                <Link className="logform-btn" to="/">Home</Link>
+            </section>
+        </>
     );
-}
+};
 
 export default Logout;
