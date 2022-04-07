35f7ec6cbfbe41eed3c0c37760ba8a47a84f1228
kowalchick <kkowalczyk91@gmail.com>
2022-04-07 18:48:53 +0200

    Install axios

--- src/components/ScrollLink.js
+++ src/components/ScrollLink.js
@@ -4,7 +4,7 @@ import {Link} from 'react-scroll';
 function ScrollLink({to,text}) {
     return (
         <li className="nav-item">
-            <Link to={to} spy={true} smooth={true} duration={500}>
+            <Link to={to} spy={true} smooth={true} duration={500} className="nav-link">
                 {text}
             </Link>
         </li>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,17 +1,43 @@
-import * as actions from './actionTypes';
+// import * as actions from './actionTypes';
+//
+// export const getSigninAction = (email, password) => ({
+//     type: actions.ACTION_SIGNIN,
+//     payload: {
+//       password,
+//       email
+//     },
+//
+// });
+// export const getSignOutAction = (email, password) => ({
+//     type: actions.ACTION_SIGNOUT,
+//     payload: {
+//
+//     },
+// });
+import { SET_CURRENT_USER, REGISTER_SUCCESS, LOGOUT, GET_ERRORS } from "./actionTypes";
 
-export const getSigninAction = (email, password) => ({
-    type: actions.ACTION_SIGNIN,
-    payload: {
-      password,
-      email
-    },
+export function setCurrentUser(user) {
+    return {
+        type: SET_CURRENT_USER,
+        payload: user
+    }
+}
 
-});
-export const getSignOutAction = (email, password) => ({
-    type: actions.ACTION_SIGNOUT,
-    payload: {
+export function registerSuccess() {
+    return {
+        type: REGISTER_SUCCESS
+    }
+}
 
-    },
-});
+export function logoutUser() {
+    return {
+        type: LOGOUT
+    }
+}
 
+export function getErrors(errors) {
+    return {
+        type: GET_ERRORS,
+        payload: errors
+    }
+}
