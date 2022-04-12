74f0d6566551007317297e1095402432f1fed469
kowalchick <kkowalczyk91@gmail.com>
2022-04-12 13:41:32 +0200

    Add the structure of Form Header Component

--- src/components/Forms/FormHeader.js
+++ src/components/Forms/FormHeader.js
@@ -1,8 +1,47 @@
 import React from 'react';
 
-function FormHeader() {
+const FormHeader = () => {
     return (
-        <div></div>
+        <header className="handform">
+            <div className="handform-wrapper">
+                <div className="handform-box-wrapper">
+                    <div className="handform-box">
+                        <div className="handform-box-title">
+                            <h2 className="fancy">Give your unwanted stuff to persons in need</h2>
+                        </div>
+                        <div className="handform-box-subtitle">
+                            It is just 4 simple steps:
+                        </div>
+                        <div className="handform-box-teps">
+                            <div className="handform-box-step">
+                                <h3>1</h3>
+                                <p>Choose things</p>
+                            </div>
+                            <div className="handform-box-step">
+                                <h3>2</h3>
+                                <p>Pack them in bags</p>
+                            </div>
+                            <div className="handform-box-step">
+                                <h3>3</h3>
+                                <p>Choose a foundation</p>
+                            </div>
+                            <div className="handform-box-step">
+                                <h3>4</h3>
+                                <p>Order a courier</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="handform-alert">
+                    <div className="container">
+                        <h1 className="alert-title">Important!</h1>
+                        <p className="alert-text">
+                            Complete the details of your stuff. Thanks to this step, we will pass them on to those who need them the most.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
