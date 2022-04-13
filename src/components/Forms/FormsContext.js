ed5cc722af254f2296137a2ffaa734cbb4e3112f
kowalchick <kkowalczyk91@gmail.com>
2022-04-13 14:59:40 +0200

    Add FormsContext component

---
+++ src/components/Forms/FormsContext.js
@@ -0,0 +1,33 @@
+import React, {useState, createContext} from "react";
+
+export const FormContext = createContext();
+
+export const FormProvider = (props) => {
+    const data = {
+        clothesToPass: false,
+        clothesToThrow: false,
+        toys: false,
+        books: false,
+        others: false,
+        bags: "--choose--",
+        localization: "--choose--",
+        whoToHelp: false,
+        customOrganization: "",
+        street: "",
+        city: "",
+        zipCode: "",
+        phoneNumber: "",
+        date: "",
+        time: "",
+        infoForTheCourier: ""
+    }
+    const [formsData, setFormsData] = useState(data);
+
+    return (
+        <FormContext.Provider value={[formsData, setFormsData]}>
+            {
+                props.children
+            }
+        </FormContext.Provider>
+    )
+}
