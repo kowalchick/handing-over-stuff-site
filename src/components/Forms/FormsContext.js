95392808e32d4cf5d281a8934cba2c0a33bc7336
Bartosz Polanczyk <bpolanczyk@sumologic.com>
2022-04-14 16:45:17 +0200

    Poprawki do kontekstu

--- src/components/Forms/FormsContext.js
+++ src/components/Forms/FormsContext.js
@@ -1,33 +1,37 @@
 import React, {useState, createContext} from "react";
 
-export const FormContext = createContext();
+export const WHAT_TO_GIVE_OPTIONS = {
+    CLOTHES_TO_PASS: 'Clothes to pass',
+    CLOTHES_TO_THROW: 'Clothes to throw',
+    TOYS: 'Toys',
+    BOOKS: 'Books',
+    OTHERS: 'Others',
+}
 
-export const FormProvider = (props) => {
-    const data = {
-        clothesToPass: false,
-        clothesToThrow: false,
-        toys: false,
-        books: false,
-        others: false,
-        bags: "--choose--",
-        localization: "--choose--",
-        whoToHelp: false,
-        customOrganization: "",
-        street: "",
-        city: "",
-        zipCode: "",
-        phoneNumber: "",
-        date: "",
-        time: "",
-        infoForTheCourier: ""
-    }
-    const [formsData, setFormsData] = useState(data);
+export const FormContext = createContext(null);
 
+const DEFAULT_DATA = {
+    whatToGive: WHAT_TO_GIVE_OPTIONS.CLOTHES_TO_PASS,
+    bags: "--choose--",
+    localization: "--choose--",
+    whoToHelp: false,
+    customOrganization: "",
+    street: "",
+    city: "",
+    zipCode: "",
+    phoneNumber: "",
+    date: "",
+    time: "",
+    infoForTheCourier: ""
+};
+
+export const FormProvider = ({children}) => {
+    const [data, setData] = useState(DEFAULT_DATA);
+
+    // FormProvider -> Provider. Context zawsze wystawia atrybut, który nazywa się "Provider" :)
     return (
-        <FormContext.FormProvider value={[formsData, setFormsData]}>
-            {
-                props.children
-            }
-        </FormContext.FormProvider>
+        <FormContext.Provider value={{data, setData}}>
+            {children}
+        </FormContext.Provider>
     )
 }
--- src/components/Forms/Variants/Variant1.js
+++ src/components/Forms/Variants/Variant1.js
@@ -1,25 +1,42 @@
 import React, {useContext} from 'react';
-// import {FormContext} from "../FormsContext";
+
+import {FormContext, WHAT_TO_GIVE_OPTIONS} from "../FormsContext";
 
 function Variant1(props) {
-    // const [formsData, setFormsData] = useContext(FormContext);
-    //
-    // const handleFormChange =(e) => {
-    //     const checked = e.target.checked;
-    //     const name = e.target.name;
-    //
-    //     setFormsData (prev => {
-    //         return{
-    //             ...prev,
-    //             [name]: checked
-    //         }
-    //     })
-    // }
+  const {data, setData} = useContext(FormContext);
 
-    return (
-        <>
-        </>
-    )
+  // Lepiej zbudować handler specyficzny dla tego, co się dzieje na danym kroku
+  // Zastanów się też gdzie lepiej trzymać buttonki - jak zaktualizować informację do następnego kroku?
+  // (walidacje real-time vs walidacje na onClick
+  const updateWhatToGive = (whatToGive) => {
+    setData({
+      ...data,
+      whatToGive
+    })
+  };
+
+  console.log(data);
+
+  // const handleFormChange =(e) => {
+  //     const checked = e.target.checked;
+  //     const name = e.target.name;
+  //
+  //     setFormsData (prev => {
+  //         return{
+  //             ...prev,
+  //             [name]: checked
+  //         }
+  //     })
+  // }
+
+  return (<>
+      VARIANT 1
+      <select onChange={evt => updateWhatToGive(evt.target.value)}>
+        {
+          Object.keys(WHAT_TO_GIVE_OPTIONS).map(key => <option key={key} value={key}>{WHAT_TO_GIVE_OPTIONS[key]}</option>)
+        }
+      </select>
+    </>)
 
 }
 
